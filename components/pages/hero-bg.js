47cbdd0b4e49d38725852a9f2a4579df1e92ad7f
Rasel Ahmed <raselwebdev@hotmail.com>
2024-12-31 17:39:16 +0300

    Modified

--- components/pages/hero-bg.js
+++ components/pages/hero-bg.js
@@ -3,10 +3,16 @@
 export default function HeroBg() {
   return (
     <div className="flex items-center justify-center h-screen bg-hero-bg absolute top-0 w-full -z-10">
-      <video className="-z-10 absolute inset-0 w-full h-full object-cover" autoPlay muted loop>
-        <source src="/videos/herobg.mp4" type="video/mp4" />
-        Your browser does not support the video tag.
-      </video>
-    </div>
+  <video
+    className="-z-10 absolute inset-0 w-full h-full object-cover pointer-events-none"
+    autoPlay
+    muted
+    loop
+  >
+    <source src="/videos/herobg.mp4" type="video/mp4" />
+    Your browser does not support the video tag.
+  </video>
+</div>
+
   );
 }
